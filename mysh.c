3a90d80074d1faeea84f5a3288a232ac9d65c374
BBodajlo <100437481+BBodajlo@users.noreply.github.com>
2023-02-28 12:47:44 -0500

    Comments on the simple code

--- mysh.c
+++ mysh.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include <string.h>
 
-static int isInitialized = 0;
+static int isInitialized = 0; //Static variable to keep track of if the shell has been initialized
 
 void initialize()
 {
     if(isInitialized == 0)
     {
-        system("clear");
+        system("clear"); //Will clear the current terminal
         printf("Welcome to the shell!!!!!!\nGood luck!!!!\n");
     }
-    isInitialized = 1;
+    isInitialized = 1; //Won't run any other time
 }
 
 
@@ -19,26 +19,26 @@ void initialize()
 int main (int argc, char** argv)
 {
 
-    initialize();
+    initialize(); //This will need to move once we get the batch mode working
     int i = 0;
-    char buff[128];
-    while(i == 0)
+    char buff[128]; //Simple start buffer for user input
+    while(i == 0) // Will probably just be set to while(1) later after logic is implemented
     {
-        printf("mysh> ");
+        printf("mysh> "); //After every command is input, the shell will give the newline prompt
         scanf("%s", buff);
 
-        if(strcmp(buff, "exit") == 0)
+        if(strcmp(buff, "exit") == 0) //Will probably move to a different logic for exiting; simple for now
         {
             i = 1;
         }
         else
         {
-            printf("ehco: %s\n", buff);
+            printf("ehco: %s\n", buff); //Just to make sure spacing worked out, the shell just ehcos back what is input
         }
 
         
     }
 
-    initialize();
+    initialize(); //Checking to see if the initilization was called again (it wasn't)
 
 }
