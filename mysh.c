50abcde8e224fec4c9f283f3be45a45cab409612
BBodajlo <100437481+BBodajlo@users.noreply.github.com>
2023-03-01 16:49:51 -0500

    Possible tokenizer template
    Will tokenize std input by putting them into a string array. Only covers spaces and end of input

--- mysh.c
+++ mysh.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <unistd.h>
+#include <fcntl.h>
 #define MAX_INPUT 128 //Max size of input
+#define MAX_TOKENS 20 //Max amount of tokens on a single line to parse
 
 static int isInitialized = 0; //Static variable to keep track of if the shell has been initialized
 
@@ -16,6 +18,9 @@ char path[MAX_INPUT] = "mysh";
 void initialize();
 void shellExit();
 void echo();
+void pwd();
+void tokenizer(char**, char*);
+void freeTokens(char**);
 
 // Command struct
 struct command
@@ -27,7 +32,8 @@ struct command
 // Command list
 struct command commandList[] = {
     {"echo", echo},
-    {"exit", shellExit}
+    {"exit", shellExit},
+    {"pwd", pwd}
 };
 
 int main (int argc, char** argv)
@@ -46,8 +52,16 @@ int main (int argc, char** argv)
         }else{
             printf("%s> ", path); 
         }
-        scanf("%s", buff); // fetches the command (input for command would not have been read yet)
+        scanf("%[^\n]", buff); // fetches the command (input for command would not have been read yet)
+        char **tokenArray = malloc(sizeof(char*)*MAX_INPUT);
+        for(int i = 0; i < MAX_TOKENS; i++)
+        {
+            tokenArray[i] = (char*)malloc(MAX_TOKENS + 1);
+        }
 
+        tokenizer(tokenArray, buff);
+        printf("%s\n",tokenArray[0]);
+        printf("%s\n",tokenArray[1]);
         // Search for command in command list
         int found = 0;
         for (int i = 0; i < sizeof(commandList) / sizeof(commandList[0]); i++)
@@ -68,7 +82,7 @@ int main (int argc, char** argv)
             // Clear stdin input since cmd non-existant 
             while (getchar() != '\n'){};
         }
-        
+        freeTokens(tokenArray); //Free token array 
     }
 
     initialize(); //Checking to see if the initilization was called again (it wasn't)
@@ -104,3 +118,57 @@ void echo()
     printf("\n");
 
 };
+void pwd()
+{
+
+};
+/*
+void tokenizer(char** input)
+{
+    char buffer[MAX_INPUT];
+    int bytes;
+
+    while((bytes = read(STDIN_FILENO, buffer, MAX_INPUT)) > 0)
+    {
+        for(int i = 0; i < bytes; i++)
+        {
+            printf("%c", buffer[i]);
+        }
+    }
+
+
+};*/
+
+//Takes the std in buffer and converts it into an array of tokens
+void tokenizer(char** tokens, char* buff)
+{
+    int tokenSpot = 0; //To increment to token array when a new token should be made
+    //Probably don't need a holder array; keeping for now
+    char holder[MAX_INPUT]; //Holder string to keep track of the current token being made
+    int holderSpot = 0; //To increment the holder spot
+    for(int i = 0; i <= strlen(buff); i++) //Goes through the std in buffer
+    {
+       
+        if(buff[i] == ' ' || i == strlen(buff)) //If the buffer has a space, or is at the end, finish the token and move to the next one
+        {
+            strcpy(tokens[tokenSpot], holder); //Copies the built token into the token array
+            holderSpot = 0; 
+            memset(holder, 0, sizeof(holder)); //Reset the holder string
+            tokenSpot +=1; //Next token
+        }
+        else{
+            holder[holderSpot++] = buff[i];
+        }
+        
+    }
+
+}
+
+void freeTokens(char** arr)
+{
+    for(int i = 0; i < MAX_TOKENS; i++)
+    {
+        free(arr[i]);
+    }
+    free(arr);
+}
